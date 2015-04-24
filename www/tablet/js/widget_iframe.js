9ab59c60b68b45414430bddeb4b0b77af3d17212
Thomas Nesges <nesges@gmail.com>
2015-04-24 17:03:22 +0200

    widget_iframe: data-check-src

--- www/tablet/js/widget_iframe.js
+++ www/tablet/js/widget_iframe.js
@@ -5,16 +5,17 @@ if(typeof widget_widget == 'undefined') {
 var widget_iframe = $.extend({}, widget_widget, {
     widgetname : 'iframe',
     init_attr: function(elem) {
-        elem.data('timeout', elem.data('timeout')||3000);
-        elem.data('src', elem.data('src'));
-        elem.data('scrolling', elem.data('scrolling') || 'no');
-        elem.data('fill', elem.data('fill') || 'no');
-        elem.data('height', elem.data('height')||100);
-        elem.data('width', elem.data('width')||100);
-        elem.data('icon-spinner', elem.data('icon-spinner')||'fa-spinner fa-spin');
-        elem.data('color-spinner', elem.data('color-spinner')||'#aa6900');
-        elem.data('icon-error', elem.data('icon-error')||'fa-frown-o');
-        elem.data('color-error', elem.data('color-error')||'#505050');
+        elem.data('src',            elem.data('src'));
+        elem.data('check-src',      elem.data('check-src')      || elem.data('src'));
+        elem.data('timeout',        elem.data('timeout')        || 3000);
+        elem.data('scrolling',      elem.data('scrolling')      || 'no');
+        elem.data('fill',           elem.data('fill')           || 'no');
+        elem.data('height',         elem.data('height')         || 100);
+        elem.data('width',          elem.data('width')          || 100);
+        elem.data('icon-spinner',   elem.data('icon-spinner')   || 'fa-spinner fa-spin');
+        elem.data('color-spinner',  elem.data('color-spinner')  || '#aa6900');
+        elem.data('icon-error',     elem.data('icon-error')     || 'fa-frown-o');
+        elem.data('color-error',    elem.data('color-error')    || '#505050');
     },
     init_ui: function(elem) {
         var spinner = $('<div />').appendTo(elem);
@@ -24,10 +25,9 @@ var widget_iframe = $.extend({}, widget_widget, {
             backgroundIcon: null,
             offColor: elem.data('color-spinner'),
         });
-
         $.ajax({
             type: 'HEAD',
-            url: elem.data('src'),
+            url: elem.data('check-src'),
             timeout: elem.data('timeout'),
             success: function(){
                 elem.empty();
@@ -40,8 +40,8 @@ var widget_iframe = $.extend({}, widget_widget, {
                 $("<iframe src='"+elem.data('src')+"' style='"+style+"border:none' scrolling='"+elem.data('scrolling')+"'/>").appendTo(elem);
             },
             error: function(x,t,m) {
-                console.log('Error trying to load '+elem.data('src')+':',t,m);
                 elem.empty();
+                console.log('Error trying to load '+elem.data('src')+':',t,'-',m);
                 var spinner = $('<div />').appendTo(elem);
                 spinner.famultibutton({
                     mode: 'signal',
