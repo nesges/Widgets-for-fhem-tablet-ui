0573c389af79db46d27c911b08d58283a4f04d07
nesges <nesges@gmail.com>
2015-05-13 00:13:32 +0200

    joinedlabel: neues Attribut mask, mpdnowplaying jetzt child von joinedlabel

--- www/tablet/js/widget_joinedlabel.js
+++ www/tablet/js/widget_joinedlabel.js
@@ -4,55 +4,60 @@ if(typeof widget_widget == 'undefined') {
 
 var widget_joinedlabel = $.extend({}, widget_widget, {
     widgetname:"joinedlabel",
+    init_attr: function(elem) {
+        elem.data('__device',   elem.data('device'));
+        elem.data('get',        elem.data('get')        || 'STATE');
+        elem.data('glue',       elem.data('glue')       || ' ');
+        elem.data('mask',       elem.data('mask')       || '');
+        
+        if(! $.isArray(elem.data('get'))) {
+            elem.data('get', new Array(elem.data('get')));
+        }
+        for(var g=0; g<elem.data('get').length; g++) {
+            var get = elem.data('get')[g];
+
+            var device = elem.data('device');
+            var reading = get;
+            
+            // advanced stuff: you could specify readings of different devices by
+            // specifiing device:reading in the get-array
+            // but this is limited by how ftui requests data from fhem: ftui collects
+            // "interesting" devices by running over data-device tags and requests data
+            // only for these. so you'd have to make shure that your devices are found 
+            // in the html code. Try this:
+            //
+            // <div data-type="joinedlabel"
+            //      data-device="DEVICE1"
+            //      data-get='["DEVICE1:readingA","DEVICE2:readingB","DEVICE3:readingC"]'
+            // ></div>
+            // <div data-device="DEVICE2"></div>
+            // <div data-device="DEVICE3"></div>
+            
+            if(get.match(/:/)) {
+                get = get.split(':');
+                device = get[0];
+                reading = get[1];
+            }
+            
+            // create a data-var with the name of the reading
+            elem.data(reading, reading);
+            // subscribe to the reading
+            readings[reading] = true;
+            
+            // save the readings device
+            elem.data('_device'+g, device);
+        }            
+    },
     init: function () {
         base=this;
         this.elements = $('div[data-type="'+this.widgetname+'"]');
         this.elements.each(function(index) {
-            $(this).data('__device', $(this).data('device'));
-            
-            $(this).data('get', $(this).data('get') || 'STATE');
-            if(! $.isArray($(this).data('get'))) {
-                $(this).data('get', new Array($(this).data('get')));
-            }
-            for(var g=0; g<$(this).data('get').length; g++) {
-                var get = $(this).data('get')[g];
-
-                var device = $(this).data('device');
-                var reading = get;
-                
-                // advanced stuff: you could specify readings of different devices by
-                // specifiing device:reading in the get-array
-                // but this is limited by how ftui requests data from fhem: ftui collects
-                // "interesting" devices by running over data-device tags and requests data
-                // only for these. so you'd have to make shure that your devices are found 
-                // in the html code. Try this:
-                //
-                // <div data-type="joinedlabel"
-                //      data-device="DEVICE1"
-                //      data-get='["DEVICE1:readingA","DEVICE2:readingB","DEVICE3:readingC"]'
-                // ></div>
-                // <div data-device="DEVICE2"></div>
-                // <div data-device="DEVICE3"></div>
-                
-                if(get.match(/:/)) {
-                    get = get.split(':');
-                    device = get[0];
-                    reading = get[1];
-                }
-                
-                // create a data-var with the name of the reading
-                $(this).data(reading, reading);
-                // subscribe to the reading
-                readings[reading] = true;
-                
-                // save the readings device
-                $(this).data('_device'+g, device);
-            }
-            
-            $(this).data('glue', $(this).data('glue') || ' ');
+            base.init_attr($(this));
         });
     },
-    
+    update_value_cb : function(value) {
+        return value;
+    },
     update: function (dev,par) {
         base=this;
         var deviceElements= this.elements.filter('div[data-device="'+dev+'"]');
@@ -74,12 +79,31 @@ var widget_joinedlabel = $.extend({}, widget_widget, {
                 
                 // get reading
                 var value = getDeviceValue($(this), reading);
+                value = base.update_value_cb(value);
                 
-                val[g] = '<span class="' + base.widgetname + '_get_' + g + '">' + value + '</span>';
+                if(value) {
+                    val[g] = '<span class="' + base.widgetname + '_get_' + g + '">' + value + '</span>';
+                }
             }
 
             $(this).empty();
-            $(this).html(val.join($(this).data('glue')));
+            if(! $(this).data('mask')) {
+                $(this).html(val.join($(this).data('glue')));
+            } else {
+                var mask = $(this).data('mask');
+                for(var g=0; g<get.length; g++) {
+                    v=1*g+1;
+                    // "[ pre $1 suf ]" 
+                    // is replaced with 
+                    // " pre " + val[1] + " suf "
+                    mask = mask.replace(new RegExp('\\[(.*?)\\$'+v+'(.*?)\\]'), (val[g]?'$1'+val[g]+'$2':''));
+                    // "$1"
+                    // is replaced with 
+                    // val[1]
+                    mask = mask.replace(new RegExp('\\$'+v), val[g]);
+                }
+                $(this).html(mask);
+            }
             $(this).data('device', $(this).data('__device'));
         });
     }
--- www/tablet/js/widget_mpdnowplaying.js
+++ www/tablet/js/widget_mpdnowplaying.js
@@ -1,63 +1,29 @@
-if(typeof widget_widget == 'undefined') {
-    loadplugin('widget_widget');
+if(typeof widget_joinedlabel == 'undefined') {
+    loadplugin('widget_joinedlabel');
 }
 
-var widget_mpdnowplaying = $.extend({}, widget_widget, {
+var widget_mpdnowplaying = $.extend({}, widget_joinedlabel, {
     widgetname:"mpdnowplaying",
     init: function () {
         base=this;
         this.elements = $('div[data-type="'+this.widgetname+'"]');
         this.elements.each(function(index) {
-            $(this).data('album', $(this).data('album') || 'album');
-            readings[$(this).data('album')] = true;
+            var album   =  typeof $(this).data('album')  != 'undefined' ? $(this).data('album')  : 'album';
+            var artist  =  typeof $(this).data('artist') != 'undefined' ? $(this).data('artist') : 'artist';
+            var name    =  typeof $(this).data('name')   != 'undefined' ? $(this).data('name')   : 'name';
+            var title   =  typeof $(this).data('title')  != 'undefined' ? $(this).data('title')  : 'title';
+            var track   =  typeof $(this).data('track')  != 'undefined' ? $(this).data('track')  : 'track';
             
-            $(this).data('artist', $(this).data('artist') || 'artist');
-            readings[$(this).data('artist')] = true;
+            $(this).data('get', new Array(name, artist, album, track, title));
+            $(this).data('mask', $(this).data('mask') || "[$1<br>][$2 ][ - $3 - ][ $4: ][ $5]");
             
-            $(this).data('name', $(this).data('name') || 'name');
-            readings[$(this).data('name')] = true;
-            
-            $(this).data('title', $(this).data('title') || 'title');
-            readings[$(this).data('title')] = true;
-            
-            $(this).data('track', $(this).data('track') || 'track');
-            readings[$(this).data('track')] = true;
+            base.init_attr($(this));
         });
     },
-    
-    update: function (dev,par) {
-        var deviceElements= this.elements.filter('div[data-device="'+dev+'"]');
-        deviceElements.each(function(index) {
-            var album       = getDeviceValue($(this), 'album');
-            var artist      = getDeviceValue($(this), 'artist');
-            var name        = getDeviceValue($(this), 'name');
-            var title       = getDeviceValue($(this), 'title');
-            var track       = getDeviceValue($(this), 'track');
-            
-            // getDeviceValue might return a timestamp which is the readings updatetime
-            // this is a bug in requestFhem not yet solved
-            // check values which most likely would not contain a timestamp and empty them
-            if(album && album.match(/^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$/)) {
-                album='';
-            }
-            if(artist && artist.match(/^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$/)) {
-                artist='';
-            }
-            if(title && title.match(/^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$/)) {
-                title='';
-            }
-            if(track && track.match(/^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$/)) {
-                track='';
-            }
-
-            $(this).empty();
-            $(this).html(
-                  (name                ? ' <span class="'+$(this).data('class-name')       +'">'   +name+' - </span>'                                :'')
-                + (artist              ? ' <span class="'+$(this).data('class-artist')     +'">'   +artist+' - </span>'                              :'')
-                + (album               ? ' <span class="'+$(this).data('class-album')      +'">'   +album+' - </span>'                               :'')
-                + (track               ? ' <span class="'+$(this).data('class-track')      +'">'   +track+' </span>'                                 :'')
-                + (title               ? ' <span class="'+$(this).data('class-title')      +'">'   +title+ '</span> '                                :'')
-            );
-        });
+    update_value_cb : function(value) {
+        if(value && value.match(/^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$/)) {
+            return '';
+        }
+        return value;
     }
 });
