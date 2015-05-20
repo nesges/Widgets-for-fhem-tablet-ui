632221f5ffd43db9b9da4731314813e1a948b510
nesges <nesges@gmail.com>
2015-05-20 23:37:50 +0200

    widget_clock: weitere formate, data-days, data-months

--- www/tablet/js/widget_clock.js
+++ www/tablet/js/widget_clock.js
@@ -4,9 +4,38 @@ if(typeof widget_widget == 'undefined') {
 
 var widget_clock = $.extend({}, widget_widget, {
     widgetname : 'clock',
+    init_attr: function(elem) {
+        elem.data('format',             elem.data('format')             || 'H:i:s');
+        elem.data('interval',           elem.data('interval')           || 1000);
+        elem.data('shortday-length',    elem.data('shortday-length')    || 3);
+        elem.data('days',               elem.data('days')               || new Array("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"));
+        elem.data('shortmonth-length',  elem.data('shortmonth-length')  || 3);
+        elem.data('months',             elem.data('months')             || new Array("Januar", "Februar", "MÃ¤rz", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"));
+        
+        
+        if(!$.isArray(elem.data('days'))) {
+            if(elem.data('days').match(/englisc?h/)) {
+                elem.data('days', new Array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"));
+            } else {
+                console.log(this.widgetname, 'init_attr', 'ERROR: data-days must be an array');
+            }
+        }
+        
+        
+        if(!$.isArray(elem.data('months'))) {
+            if(elem.data('months').match(/englisc?h/)) {
+                elem.data('months', new Array("January","February","March","April","May","June","July","August","September","October","November","December"));
+            } else {
+                console.log(this.widgetname, 'init_attr', 'ERROR: data-months must be an array');
+            }
+        }
+        
+    },
     init: function () {
+        base = this;
         this.elements = $('div[data-type="'+this.widgetname+'"]');
         this.elements.each(function(index) {
+            base.init_attr($(this));
             var f = function() {
                 var d = new Array();
                 now = new Date();
@@ -20,10 +49,25 @@ var widget_clock = $.extend({}, widget_widget, {
                 // G: Stunde im 24-Stunden-Format, ohne fÃ¼hrender Null
                 // i: Minute der Stunde, mit fÃ¼hrender Null
                 // s: Sekunde der Minute, mit fÃ¼hrender Null
-                // w: Wochentagszahl (Sonntag = 0)
                 // u: Millisekunden mit fÃ¼hrender Null
                 // O: Zeitunterschied zur Greenwich time (GMT) in Stunden
                 // U: Sekunden seit Beginn der UNIX-Epoche (January 1 1970 00:00:00 GMT)
+                // w: Wochentagszahl (Sonntag = 0, Samstag = 6)
+                // N: Wochentagszahl nach ISO-8601 (Montag = 1, Sonntag = 7)
+                // l: Wochentag
+                // D: Wochentag gekürzt
+                // S: Anhang der englischen Aufzählung für einen Monatstag, zwei Zeichen
+                // F: Monat als ganzes Wort, wie January oder March
+                // M: Monatsname gekürzt
+                // g: Stunde im 12-Stunden-Format, ohne führende Nullen
+                // h: Stunde im 12-Stunden-Format, mit führenden Nullen
+                // a: am/pm
+                // A: AM/PM
+                
+                // TODO:
+                // z: Der Tag des Jahres
+                // W: ISO-8601 Wochennummer des Jahres
+                
                 
                 d['Y'] = now.getFullYear();
                 d['n'] = now.getMonth()+1;
@@ -43,16 +87,33 @@ var widget_clock = $.extend({}, widget_widget, {
                 d['i'] = d['i']<10?'0'+d['i']:d['i'];
                 d['s'] = d['s']<10?'0'+d['s']:d['s'];
                 d['u'] = d['u']<10?'000'+d['u']:d['u']<100?'00'+d['u']:d['u']<1000?'0'+d['u']:d['u'];
+                d['N'] = d['w']==0?7:d['w'];
+                d['l'] = f.elem.data('days')[(d['N']-1)];
+                d['D'] = d['l'].substr(0,f.elem.data('shortday-length'));
+                d['S'] = String(d['j']).match(/[23]?1$/)?'st':String(d['j']).match(/[23]?2$/)?'nd':String(d['j']).match(/[23]?3$/)?'rd':'th';
+                d['F'] = f.elem.data('months')[(d['n']-1)];
+                d['M'] = d['F'].substr(0,f.elem.data('shortmonth-length'));
+                d['g'] = d['G']<=12?d['G']:d['G']-12;
+                d['h'] = d['g']<10?'0'+d['g']:d['g'];
+                d['a'] = d['G']<=12?'am':'pm';
+                d['A'] = d['a'].toUpperCase();
                 
-                datestring = f.format;
-                for ( var key in d ) {
-                    datestring = datestring.replace(key, d[key]);
-                };
-                f.elem.text(datestring);
+                // split formatstring into it's letters and replace one after the other
+                var datearr = f.format.split('');
+                for(l=0; l<datearr.length; l++) {
+                    for ( var key in d ) {
+                        if(datearr[l] == key) {
+                            datearr[l] = d[key];
+                            // stop replacing after a match
+                            break;
+                        }
+                    }
+                }
+                f.elem.text(datearr.join(''));
             };
             f.elem = $(this);
-            f.format = $(this).attr("data-format")||'H:i:s';
-            setInterval(f, $(this).attr("data-interval")||1000);
+            f.format = $(this).data('format');
+            setInterval(f, $(this).data('interval'));
         });
     },
     update: function (dev,par) {}
